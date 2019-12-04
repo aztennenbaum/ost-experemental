877e70634e59d6714e7f03b95966c2730e119694
aztennenbaum <aztennenbaum@gmail.com>
2019-12-03 21:32:35 -0500

    Update svd2b.m

--- doc/svd2b.m
+++ doc/svd2b.m
@@ -1,27 +1,28 @@
-function [ U, S, V ] = svd2b( X )
+function [ U, S, V] = svd2( X )
 %Closed form svd for 2x2 complex matrices
     X=X/sqrt(det(X));
     P = X*X';
-    a=P(1,1);
-    b=P(1,2);
-    c=P(2,1);
-    d=P(2,2);
     
-    sgn1=abs(a-d)/(a-d);%abs(a-d) = 0 -> pure rotation
+    s_order=sign_nz(P(1,1)-P(2,2));
+    tr=trace(P);
     
-    root = sgn1*((a+d)^2-4)^(1/2);
-    s1 = (a + d + root)/2;
-    s2 = (a + d - root)/2;
-    U = sgn1*[a-s2 -b;c s1-d];
+    root = s_order*(tr^2-4)^(1/2);
+    Sp_sq = [tr-root 0;0 tr+root]/2;
+    
+    U = s_order*(P-Sp_sq)*[1 0;0 -1];
     U = U/sqrt(det(U));
-    S = [sqrt(s1) 0;0 sqrt(s2)];
-    V = (inv(S)*U'*X)';
     
-    sgn2=abs(trace(V))/trace(V);
     
-    S = sgn2*S;
-    V = sgn2*V;
-    mobius2q(U)
-    mobius2q(V)
+    Sp = Sp_sq.^(1/2);
+    S  = inv(Sp);
+    V  = (Sp*U'*X)';
+    
+%     uq = mobius2q(U);
+%     E = [(norm(mobius2q(X-U*S*V')));...
+%          (norm(imag(mobius2q(U))));...
+%           norm(uq(3));...
+%          (norm(imag(mobius2q(V))))];
+%      ecount = 0 ; for a=[1:100000]; [u s v e] = svd2(randn(2,2)+1i*randn(2,2)); if any(e>eps*10) ecount=ecount+1; end; end; ecount
+
 end
 
