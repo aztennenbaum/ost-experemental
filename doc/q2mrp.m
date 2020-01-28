bbc231ed64fff4909ea2c23b874c0222c3164574
aztennenbaum <aztennenbaum@gmail.com>
2020-01-27 21:09:59 -0500

    so beautiful

--- doc/q2mrp.m
+++ doc/q2mrp.m
@@ -1,4 +1,4 @@
 function [ mrp ] = q2mrp( q)
-% Convert quaternion to modified rodrigues parameter
-mrp=q(1:3)/(q(4)+1);
+% Convert unnormalized quaternion to modified rodrigues parameter
+mrp=q(1:3)/(q(4)+sign_nz(q(4))*norm(q));
 end
